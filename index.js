c83aadbce2112ea2a94a4877b98b1833c3bf998d
Jonathan Hsieh <jonathan@Jonathans-MacBook-Pro.local>
2019-08-14 09:40:11 -0700

    set up proxy for deployment

--- index.js
+++ index.js
@@ -1,59 +1,32 @@
 const express = require('express');
-const httpProxy = require('http-proxy');
-const apiProxy = httpProxy.createProxyServer();
-const serverPhotos = 'http://3.14.5.145';
-const serverCalendar = 'http://18.188.235.153';
-const serverListing = 'http://18.221.218.103';
-const serverReviews = 'http://ec2-13-57-195-146.us-west-1.compute.amazonaws.com';
+const axios = require('axios');
 const app = express();
 const port = 3005;
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 app.use(bodyParser());
-app.use(morgan());
+app.use(morgan('dev'));
 app.use('/:listingID', express.static('./public/dist'));
 
-app.all('/listing/desc/:listingID', (req, res) => {
-  apiProxy.web(req, res, {
-    target: serverListing
-  });
+app.all('/:listingID/desc', (req, res) => {
+  let { listingID } = req.params;
+  axios.get(`http://ec2-52-14-23-211.us-east-2.compute.amazonaws.com/${listingID}/desc`)
+    .then(({ data }) => res.status(200).send(data))
+    .catch(err => res.status(500).send(err));
 })
 
-app.all('/listing/amenity/:listingID', (req, res) => {
-  apiProxy.web(req, res, {
-    target: serverListing
-  });
+app.all('/:listingID/basic-amen', (req, res) => {
+  let { listingID } = req.params;
+  axios.get(`http://ec2-52-14-23-211.us-east-2.compute.amazonaws.com/${listingID}/basic-amen`)
+    .then(({ data }) => res.status(200).send(data))
+    .catch(err => res.status(500).send(err));
 })
 
-app.all('/listing/:listingID', (req, res) => {
-  apiProxy.web(req, res, {
-    target: serverCalendar
-  });
+app.all('/:listingID/special-amen', (req, res) => {
+  let { listingID } = req.params;
+  axios.get(`http://ec2-52-14-23-211.us-east-2.compute.amazonaws.com/${listingID}/special-amen`)
+    .then(({ data }) => res.status(200).send(data))
+    .catch(err => res.status(500).send(err));
 })
-
-app.all('/custom/month', (req, res) => {
-  apiProxy.web(req, res, {
-    target: serverCalendar
-  });
-})
-
-app.all('/reserved/month', (req, res) => {
-  apiProxy.web(req, res, {
-    target: serverCalendar
-  });
-})
-
-app.all('/api/listings/photos/:listingID', (req, res) => {
-  apiProxy.web(req, res, {
-    target: serverPhotos
-  });
-})
-
-app.all("/api/listings/photos/initial/:listingID", function(req, res) {
-  apiProxy.web(req, res, 
-    {target: serverPhotos
-  });
-});
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
